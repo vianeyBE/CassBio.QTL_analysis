6786e13e0e5120160531300648c9d61af6422134
VianeyBarrera <vpbarrerae@gmail.com>
2023-03-29 09:22:48 -0500

    single QTL pipeline
    - single qtl as function
    - add code for plots

--- QTL_Analysis.SingleQTL.R
+++ QTL_Analysis.SingleQTL.R
@@ -1,163 +1,260 @@
-library("qtl")
+# QTL Analysis using Rqtl package
+# Author: Vianey Barrera (vpbarrerae@gmail.com / v.barrera@cgiar.org)
+# The script runs the single QTL analysis (scanone) using data in mapqtl format.
+# It computes QTLs, its Lod/Bayes intervals, scores and effects
+# arguments:
+# dircross: dir were data is located. It can be an empty string if the cross files include the path and name
+# dir: dir to save results.
+# locfile: .loc file name, it can include the path
+# mapfile: .map file name, it can include the path
+# phenofile: .qua file name, it can include the path
+# prefixResults: Results will have this prefix
+# ncores: number of cores for permutation test
+# step <- 0.5 : scanone arguments
+# off.end <- 0 : scanone arguments
+# error.prob <- 0.001 : scanone arguments
+# alpha <- 0.1 : scanone arguments
+# map.function <- "kosambi" : scanone arguments
+# stepwidth <- "fixed" : scanone arguments
+# model_scanone <- "normal" : scanone arguments
+# n.perm <- 1000 : scanone arguments - For permutation test
 
-prefixResults <- "CM8996_2023" 
-dir <- ""
-#dir <- choose.dir(default = "", caption = "Select folder")
-locfile <- gsub("\\\\", "/", file.choose()) 
-mapfile <- gsub("\\\\", "/", file.choose()) 
-phenofile <-gsub("\\\\", "/", file.choose()) 
 
 
-cross = read.cross(format = "mapqtl", 
-                 dir = dir, 
-                 genfile = locfile,
-                 mapfile = mapfile,
-                 phefile = phenofile, 
-                 genotypes = NULL)
-cross = jittermap(cross)
-cross = drop.nullmarkers(cross)
-
-nind = nind(cross)
-npheno = nphe(cross)
-nmarker =totmar(cross)
-nchro = nchr(cross)
-ngeno = length(levels(as.factor(cross$geno[[1]][["data"]])))
-
-# Genetic map data
-data.map = pull.map(cross, as.table=TRUE)
-data.map = cbind(mar = row.names(data.map), data.frame(data.map))
-row.names(data.map) = c()
-
-# Phenotype data
-data.pheno = cross[["pheno"]]
-phenotypes = colnames(data.pheno)
-
-## Interval mapping - Standard Mapping ####
-cross = calc.genoprob(cross, step = 0.5, error.prob = 0.001)
-out = scanone(cross, method = "em", pheno.col=c(2:dim(cross$pheno)[2]))
-
-## Permutation Test ####
-out.perm <- scanone(cross, method = "em", n.perm = 1000,  pheno.col=c(2:dim(cross$pheno)[2]), n.cluster=ncores)
-summary(out.perm, alpha=c(0.10, 0.05, 0.01)) 
-
-# Stablish threshold 
-alpha = 0.1
-lod.threshold = melt(as.data.frame(print(summary(out.perm, alpha = alpha))), 
-                     value.name = "lod", variable.name = "phenotype")
-
-# Estimate picks 
-peaks = (summary(object = out, 
-                      perms = out.perm, 
-                      format ="allpheno", threshold = as.numeric(lod.threshold[,2]))) %>% 
-  melt(id.vars = c("chr", "pos"), value.name = "lod", variable.name = "phenotype") %>%
-  merge(lod.threshold, by = "phenotype", suffixes = c("", ".threshold") ) %>% 
-  subset(lod > lod.threshold)
-peaks$pheno_num = peaks$phenotype
-levels(peaks$pheno_num) = c(1:length(levels(peaks$phenotype)))
-
-#Estimate Intervals - LOD method ####
-lodint = matrix(ncol=10, nrow = 0)
-colnames(lodint) =  c("phenotype", "pheno_num", "chr", "lod", 
-                        "start.marker", "start.pos", 
-                        "max.marker", "max.pos", 
-                        "end.marker", "end.pos")
-
-for (i in 1:dim(peaks)[1]){
-  lodint.single = lodint(out, chr = as.character(peaks$chr[i]), lodcolumn=as.numeric(peaks$pheno_num[i]), 
-                         expandtomarkers = TRUE, drop = 1)
-  lodint.temp = data.frame(phenotype = as.character(peaks$phenotype[i]),
-                      pheno_num = as.numeric(peaks$pheno_num[i]), 
-                      chr = as.character(peaks$chr[i]), 
-                      lod = peaks$lod[i], 
-                      start.marker = row.names(lodint.single)[1], 
-                      start.pos = lodint.single[1,2], 
-                      max.marker = row.names(lodint.single)[2], 
-                      max.pos = lodint.single[2,2], 
-                      end.marker = row.names(lodint.single)[3], 
-                      end.pos = lodint.single[3,2])
-  lodint = rbind(lodint, lodint.temp)
+single_qtl <- function(dircross, dir, locfile, mapfile, phenofile, prefixResults, 
+                       ncores, step=0.5, off.end=0, error.prob=0.001, alpha=0.1, n.perm=1000,
+                       map.function="kosambi", stepwidth="fixed", model_scanone="normal"){
+  
+  # Loading Packages and variables ------------------------------------------
+  
+  library("qtl")
+  library("tidyverse")
+  library("snow")
+  
+  options(warn=-1)
+  
+  # Reading Cross  ----------------------------------------------------------
+  
+  cross <- read.cross(format = "mapqtl", 
+                      dir = dircross, 
+                      genfile = locfile,
+                      mapfile = mapfile,
+                      phefile = phenofile, 
+                      genotypes = NULL)
+  
+  message("Data was loaded successfully!")
+  
+  cross <- jittermap(cross)
+  cross <- drop.nullmarkers(cross)
+  
+  nind <- nind(cross)
+  message(paste0("Number of individuals: ", nind))
+  npheno <- nphe(cross)
+  message(paste0("Number of traits: ", npheno-1))
+  nmarker <- totmar(cross)
+  message(paste0("Number of SNPs: ", nmarker))
+  nchro <- nchr(cross)
+  message(paste0("Number of chromosomes: ", nchro))
+  
+  #ngeno = length(levels(as.factor(cross$geno[[1]][["data"]])))
+  
+  # Genetic map data
+  data.map <- pull.map(cross, as.table=TRUE) %>% rownames_to_column(var='mar')
+  
+  # Phenotype data
+  data.pheno <- cross[["pheno"]]
+  phenotypes <- colnames(data.pheno)
+  
+  
+  # Interval Mapping --------------------------------------------------------
+  
+  cross <- calc.genoprob(cross, map.function = map.function, stepwidth = stepwidth,
+                         step = step, error.prob = error.prob, off.end = off.end)
+  
+  out <- scanone(cross, method = "em", model = model_scanone,
+                 pheno.col = c(2:dim(cross$pheno)[2]))
+  
+  message("Interval Mapping: done!")
+  
+  # Permutation Test --------------------------------------------------------
+  
+  message(paste0("Number of interations for permutation test: ", n.perm))
+  
+  out.perm <- scanone(cross, method = "em", n.perm = n.perm, n.cluster = ncores, 
+                      pheno.col = c(2:dim(cross$pheno)[2]))
+  
+  message(paste0(capture.output(summary(out.perm, alpha = c(0.10, 0.05, 0.01))), collapse = "\n"))
+  
+  message("Permutation Test: done!")
+  
+  
+  # Finding Peaks -----------------------------------------------------------
+  
+  # threshold
+  lod.threshold <- melt(as.data.frame(print(summary(out.perm, alpha = alpha))), 
+                        value.name = "lod", variable.name = "phenotype")
+  
+  message("Finding Peaks...")
+  
+  # Estimate picks 
+  peaks <- (summary(object = out, 
+                    perms = out.perm, format ="allpheno", 
+                    threshold = as.numeric(lod.threshold[,2]))) %>% 
+    melt(id.vars = c("chr", "pos"), 
+         value.name = "lod", variable.name = "phenotype") %>%
+    merge(lod.threshold, by = "phenotype", suffixes = c("", ".threshold") ) %>% 
+    subset(lod > lod.threshold)
+  
+  peaks$pheno_num <- peaks$phenotype
+  
+  levels(peaks$pheno_num) <- c(1:length(levels(peaks$phenotype)))
+  
+  message(paste0("Peaks detected: ", dim(peaks)[1] ))
+  
+  # Estimate Intervals ------------------------------------------------------
+  
+  message("Estimating Intervals using LOD and Bayes method...")
+  
+  colnames_intervals <- c("phenotype", "pheno_num", "chr", "lod", 
+                          "start.marker", "start.pos", 
+                          "max.marker", "max.pos", 
+                          "end.marker", "end.pos")
+  
+  # Estimate Intervals - LOD method
+  lodint <- matrix(ncol=10, nrow = 0)
+  colnames(lodint) <- colnames_intervals
+  
+  for (i in 1:dim(peaks)[1]){
+    lodint.single <- lodint(out, chr = as.character(peaks$chr[i]), 
+                            lodcolumn=as.numeric(peaks$pheno_num[i]), 
+                            expandtomarkers = TRUE, drop = 1)
+    
+    lodint.temp <- data.frame(phenotype = as.character(peaks$phenotype[i]),
+                              pheno_num = as.numeric(peaks$pheno_num[i]), 
+                              chr = as.character(peaks$chr[i]), 
+                              lod = peaks$lod[i], 
+                              start.marker = row.names(lodint.single)[1], 
+                              start.pos = lodint.single[1,2], 
+                              max.marker = row.names(lodint.single)[2], 
+                              max.pos = lodint.single[2,2], 
+                              end.marker = row.names(lodint.single)[3], 
+                              end.pos = lodint.single[3,2])
+    
+    lodint <- rbind(lodint, lodint.temp)
+  }
+  
+  lodint <- distinct(lodint, pheno_num, chr, start.pos, max.pos, end.pos, .keep_all = TRUE) 
+  
+  # Find markers
+  for (i in 1:dim(lodint)[1]){
+    lodint[i,7] <- find.marker(cross, chr = lodint[i,3], pos = lodint[i,8])
+  }
+  
+  # Estimate Intervals - BAYES method
+  bayesint <- matrix(ncol=10, nrow = 0)
+  
+  colnames(bayesint) <- colnames_intervals
+  
+  for (i in 1:dim(peaks)[1]){
+    bayesint.single = bayesint(out, chr = as.character(peaks$chr[i]), 
+                               lodcolumn = as.numeric(peaks$pheno_num[i]), 
+                               expandtomarkers = TRUE)
+    
+    bayesint.temp = data.frame(phenotype = as.character(peaks$phenotype[i]),
+                               pheno_num = as.numeric(peaks$pheno_num[i]), 
+                               chr = as.character(peaks$chr[i]), 
+                               lod = peaks$lod[i], 
+                               start.marker = row.names(bayesint.single)[1], 
+                               start.pos = bayesint.single[1,2], 
+                               max.marker = row.names(bayesint.single)[2], 
+                               max.pos = bayesint.single[2,2], 
+                               end.marker = row.names(bayesint.single)[3], 
+                               end.pos = bayesint.single[3,2])
+    
+    bayesint = rbind(bayesint, bayesint.temp)
+  }
+  
+  bayesint <- distinct(bayesint, pheno_num, chr, start.pos, max.pos, end.pos, .keep_all = TRUE) 
+  
+  # Find markers
+  for (i in 1:dim(lodint)[1]){
+    bayesint[i,7] <-  find.marker(cross, chr = bayesint[i,3], pos = bayesint[i,8])
+  }
+  
+  
+  # QTL Effects -------------------------------------------------------------
+  
+  message("Computing QTL Effects")
+  
+  qtl.effects <-  cbind(lodint[,c(1,3,4,7)], additive = NA, dominance = NA, ratio = NA)
+  
+  for (i in 1:dim(lodint)[1]){
+    
+    mar <-  find.marker(cross, chr=lodint$chr[i], pos = lodint$max.pos[i])
+    eff <-  effectplot(cross, mname1=mar, pheno.col = lodint$pheno_num[i]+1, draw = FALSE)
+    qtl.effects$additive[i] <- (eff[["Means"]][1] - eff[["Means"]][4])/2
+    qtl.effects$dominance[i] <- (eff[["Means"]][2] + eff[["Means"]][3])/2 - (eff[["Means"]][1] + eff[["Means"]][4])/2
+    effectplot(cross, mname1=mar, pheno.col = lodint$pheno_num[i]+1)
+    plotPXG(cross, marker = mar, pheno.col = lodint$pheno_num[i]+1)
+    
+  }
+  
+  qtl.effects$ratio = qtl.effects$dominance/abs(qtl.effects$additive)
+  
+  
+  # Plots -------------------------------------------------------------------
+  
+  outputdir <- paste0(dir, prefixResults,  "_Results/")
+  outputplot <- paste0(dir, prefixResults,  "_Plots/")
+  dir.create(outputdir, showWarnings = FALSE)
+  dir.create(outputplot, showWarnings = FALSE)
+  
+  message(paste0("Saving Plots at: ", outputdir, " and ", outputplot))
+  
+  source("QTL_Analysis.singleQTL_plot.R")
+  genoPlot.pdf(cross)
+  missGenoPlot.pdf(cross)
+  plotPhenotypes.pdf(data.pheno)
+  plotLOD.pdf(out)
+  
+  source("QTL_Analysis.singleQTL_map_plot.R") 
+  plot.single.QTL(lodint, "Lod")
+  plot.single.QTL(bayesint, "Bayes")
+  
+  
+  # Saving Data -------------------------------------------------------------
+  
+  message("Saving Data...")
+  
+  # RData
+  save.image(paste(prefixResults, format(Sys.Date(), "%m%d%Y"), "RData", sep = "."))
+  
+  
+  # Save Files and Plots ####
+  
+  write.table(lodint,
+              file = paste(outputdir, prefixResults, ".qtl.LodIntervals.txt", sep = ""), sep = "\t", 
+              row.names = FALSE, quote = FALSE )
+  write.table(bayesint,
+              file = paste(outputdir, prefixResults, ".qtl.BayesIntervals.txt", sep = ""), sep = "\t", 
+              row.names = FALSE, quote = FALSE )
+  write.table(out,
+              file = paste(outputdir, prefixResults, ".lod.scores.txt", sep = ""), sep = "\t", 
+              row.names = TRUE, quote = FALSE )
+  write.table(qtl.effects,
+              file = paste(outputdir, prefixResults, ".qtl.effects.txt", sep = ""), sep = "\t", 
+              row.names = FALSE, quote = FALSE )
+  
+  
+  message("Done!")
+  
+  
 }
 
-lodint <- distinct(lodint,pheno_num,chr,start.pos,max.pos,end.pos,.keep_all = TRUE) 
-
-# Find markers
-for (i in 1:dim(lodint)[1]){
-  lodint[i,7] = find.marker(cross, chr = lodint[i,3], pos = lodint[i,8])
-}
-
-#Estimate Intervals - BAYES method ####
-bayesint = matrix(ncol=10, nrow = 0)
-colnames(bayesint) =  c("phenotype", "pheno_num", "chr", "lod", 
-                        "start.marker", "start.pos", 
-                        "max.marker", "max.pos", 
-                        "end.marker", "end.pos")
-
-for (i in 1:dim(peaks)[1]){
-  bayesint.single = bayesint(out, chr = as.character(peaks$chr[i]), lodcolumn=as.numeric(peaks$pheno_num[i]), 
-                             expandtomarkers = TRUE)
-  bayesint.temp = data.frame(phenotype = as.character(peaks$phenotype[i]),
-                             pheno_num = as.numeric(peaks$pheno_num[i]), 
-                             chr = as.character(peaks$chr[i]), 
-                             lod = peaks$lod[i], 
-                             start.marker = row.names(bayesint.single)[1], 
-                             start.pos = bayesint.single[1,2], 
-                             max.marker = row.names(bayesint.single)[2], 
-                             max.pos = bayesint.single[2,2], 
-                             end.marker = row.names(bayesint.single)[3], 
-                             end.pos = bayesint.single[3,2])
-  bayesint = rbind(bayesint, bayesint.temp)
-}
-
-bayesint <- distinct(bayesint,pheno_num,chr,start.pos,max.pos,end.pos,.keep_all = TRUE) 
-
-# Find markers
-for (i in 1:dim(lodint)[1]){
-  bayesint[i,7] = find.marker(cross, chr = bayesint[i,3], pos = bayesint[i,8])
-}
-
-# QTL Effects ####
-qtl.effects = cbind(lodint[,c(1,3,4,7)], additive = NA, dominance = NA, ratio = NA)
-
-for (i in 1:dim(lodint)[1]){
-  mar = find.marker(cross, chr=lodint$chr[i], pos = lodint$max.pos[i])
-  eff = effectplot(cross, mname1=mar, pheno.col = lodint$pheno_num[i]+1, draw = FALSE)
-  qtl.effects$additive[i] = (eff[["Means"]][1] - eff[["Means"]][4])/2
-  qtl.effects$dominance[i] = (eff[["Means"]][2] + eff[["Means"]][3])/2 - (eff[["Means"]][1] + eff[["Means"]][4])/2
-  effectplot(cross, mname1=mar, pheno.col = lodint$pheno_num[i]+1)
-  plotPXG(cross, marker = mar, pheno.col = lodint$pheno_num[i]+1)
-}
-
-effectplot(cross, mname1="C07_8723674_C", pheno.col = 1+1)
-plotPXG(cross, marker = "C07_13735570_G", pheno.col = 1+1)
 
 
-qtl.effects$ratio = qtl.effects$dominance/abs(qtl.effects$additive)
-
-#save.image(paste(prefixResults, ".RData", sep = ""))
-
-# Save Files and Plots ####
-dir.create(paste(dir, "results", sep = ""))
-dir.create(paste(dir, "plots", sep = ""))
-
-write.table(lodint, file = paste(dir, "results/", prefixResults, ".qtl.LodIntervals.txt", sep = ""), sep = "\t", 
-            row.names = FALSE, quote = FALSE )
-write.table(bayesint, file = paste(dir, "results/", prefixResults, ".qtl.BayesIntervals.txt", sep = ""), sep = "\t", 
-            row.names = FALSE, quote = FALSE )
-write.table(out, file = paste(dir, "results/", prefixResults, ".lod.scores.txt", sep = ""), sep = "\t", 
-            row.names = TRUE, quote = FALSE )
-write.table(qtl.effects, file = paste(dir, "results/", prefixResults, ".qtl.effects.txt", sep = ""), sep = "\t", 
-            row.names = FALSE, quote = FALSE )
 
 
-# Better to run in computer instead of server because some libraries are not possible to update in the server
-source("qtl-plots-functions.R")
-genoPlot.pdf(cross)
-missGenoPlot.pdf(cross)
-plotPhenotypes.pdf(data.pheno)
-plotLOD.pdf(out)
 
-source("qtl-map-plot.single-qtl.R") 
-plot.single.QTL(lodint, "Lod")
-plot.single.QTL(bayesint, "Bayes")
 
 
---
+++ QTL_Analysis.singleQTL_map_plot.R
@@ -0,0 +1,225 @@
+# The function create PDF files with the chromosome, interval and LOD profile
+
+plot.single.QTL = function(qtldata, int_method){
+  
+  library(RColorBrewer)
+  library(ggtext)
+  
+  options(warn=-1)
+  
+  # Preparing Information ------------------------------------------------------
+
+  # map data
+  data.map = matrix(ncol = 3, nrow=0)
+  colnames(data.map) = c("mar", "pos", "chr")
+  
+  # chromosome size
+  chr.size = matrix(ncol = 2, nrow=0)
+  colnames(chr.size) = c("chromosome", "size")
+  
+  # LG names
+  nameschro = names(cross[["geno"]])
+  
+  for (i in c(1:nchro)){
+    
+    # map
+    chr.map = cross[["geno"]][[i]][["map"]][1,]
+    temp.map = data.frame(mar = names(chr.map), 
+                          chr = (rep(nameschro[i], length(chr.map)[1])),
+                          pos = chr.map, 
+                          row.names=NULL)
+    data.map = rbind(data.map, temp.map)
+    
+    # chr
+    chr.size.temp = data.frame(chromosome=nameschro[i], size=max(temp.map$pos))
+    chr.size = rbind(chr.size, chr.size.temp)
+  }
+  
+
+  # Reading each Chromosome ----------------------------------------------------
+
+  for (LG in unique(qtldata$chr)){
+    
+    message(paste0("Creating plot for chromosome ", LG))
+    
+    data.map.qtl = subset(data.map, chr == LG) # Get data of the map
+    chr.size.qtl = subset(chr.size, chromosome == LG ) # Get data of the size
+    data.qtl = subset(qtldata, chr == LG) # Get qtls on the LG
+    mypal.qtl = brewer.pal(n = dim(data.qtl)[1], name = "Dark2")[1:dim(data.qtl)[1]] 
+
+    qtl.label = as.data.frame(rbind(cbind(data.qtl$start.marker, data.qtl$start.pos),
+                      cbind(data.qtl$max.marker, data.qtl$max.pos),
+                      cbind(data.qtl$end.marker, data.qtl$end.pos)
+                      ))
+    colnames(qtl.label)= c("markers", "pos")
+    qtl.label$pos = as.numeric(qtl.label$pos)
+    
+    
+
+    # Plot Chromosome ---------------------------------------------------------
+
+    # Axis 
+    wide.chr = 0.75
+    brk  = 5
+    my_breaks = seq(0, round(max(chr.size.qtl$size), digits = -1)+brk, by = brk)
+    my_labs = interleave(seq(0, round(max(chr.size.qtl$size), digits = -1), by = brk*2), "")
+    
+    # Plots a basic plot with the chromosome, the SNPs, QTL intervals, and labels 
+    # of flank SNPs inside the chromosome
+    qtlmap.basic = function(chr.size.qtl, data.map.qtl, data.qtl){
+      qtl.map = 
+        ggplot() + 
+        scale_x_discrete(name ="", # Axis
+                         position = "top",
+                         expand = expansion(add = c(1, 1))
+        ) + 
+        scale_y_reverse(name="Location (cM)", 
+                        expand = expansion(mult = c(0, 0)),
+                        breaks = my_breaks,
+                        labels = my_labs
+        ) +
+        geom_rect(data=chr.size.qtl, #Draw chromosomes
+                  aes(xmin=1-wide.chr/3,
+                      xmax=1+wide.chr/3,
+                      ymin=0, 
+                      ymax=size+2), 
+                  alpha = 0, 
+                  color="grey20"
+        ) +
+        geom_rect(data=data.map.qtl, #Draw SNPs
+                  aes(xmin=c(rep(1, dim(data.map.qtl)[1]))-wide.chr/3,
+                      xmax=c(rep(1, dim(data.map.qtl)[1]))+wide.chr/3,
+                      ymin=pos-0.001,
+                      ymax=pos+0.001),
+                  alpha = 0.65,
+                  color = "black",
+                  fill = "black"
+        ) +
+        geom_rect(data=data.qtl, #Draw qtl intervals
+                  aes(xmin=c(rep(1,dim(data.qtl)[1]))-wide.chr/3,
+                      xmax=c(rep(1,dim(data.qtl)[1]))+wide.chr/3,
+                      ymin=start.pos,
+                      ymax=end.pos),
+                  alpha = 0.2,
+                  fill = mypal.qtl,
+                  color = "black"
+        ) +
+        theme(panel.background = element_blank(), # Theme
+              axis.ticks.x = element_blank(),
+              axis.ticks.length = unit(.5, "cm"),
+              axis.text.x = element_text(size = 24, face = "bold"),
+              axis.text.y = element_text(size = 24, face = "bold", color = "black", margin=margin(0,0,0,20)),
+              text = element_text(size = 24, face = "bold"),
+              axis.line.y.left = element_line(color = "black", size = 1)
+        ) +
+        ggrepel::geom_text_repel(data=qtl.label, #Labels 
+                               mapping = aes(x = c(rep(1, dim(qtl.label)[1])), 
+                                             y = pos,
+                                             label = markers),
+                               size = 14,
+                               nudge_x = -0.75,
+                               direction = "y"
+                               
+        )
+      return(qtl.map)
+    }
+    
+    qtl.map = qtlmap.basic(chr.size.qtl, data.map.qtl, data.qtl)
+    
+  
+    # Plot Interval and Label -------------------------------------------------
+
+    data.qtl$xvalue <- 1+wide.chr*0.4 + seq(from=0, to=0.1*(dim(data.qtl)[1]-1), by=0.1)
+    
+    qtl_range <- list()
+    qtl_text <- list()
+    
+    for ( i in 1:dim(data.qtl)[1] ){
+      
+      p <- geom_pointrange(data = data.qtl[i,],
+                           aes(x = xvalue, y = max.pos, ymin = start.pos, ymax = end.pos),
+                           size = 2, shape = 16, alpha = 1, fatten = 2,colour = mypal.qtl[i])
+      
+      q <- geom_richtext(data = data.qtl[i,],
+                         aes(x = xvalue, 
+                             y = end.pos, # it might be necessary to move label (!)
+                             label = phenotype, angle = -90, hjust = -0.05),
+                         label.color = NA, fill = NA, text.colour = mypal.qtl[i], size = 14)
+      qtl_range[[i]] <- p
+      qtl_text[[i]] <- q
+    
+    }
+    
+    qtl.map <- qtl.map + list(qtl_range) + list(qtl_text)
+  
+    # LOD Profile --------------------------------------------------------------
+    qtl.lod.temp = data.frame(loci =  row.names(out),
+                         chr = as.character(out$chr),
+                         pos = out[,2],
+                         out[,c(data.qtl$phenotype)])
+    
+    qtl.lod.temp = subset(qtl.lod.temp, chr == LG)
+    
+    
+    plotLODmqm = function(data.lod.temp){
+        row.names(data.lod.temp) = c()
+        maxlod = max(as.data.frame(data.lod.temp[,4:dim(data.lod.temp)[2]]))
+        data.lod = melt(data.lod.temp, id.vars = c("loci", "chr", "pos"))
+        data.lod.markers = subset(data.lod, loci %in% data.map$mar)
+        
+        my_breaks = seq(0, round(maxlod)+0.5,by=0.5)
+        my_labs = interleave(seq(0,round(maxlod), by=1), "")
+        #data.lod = melt(data.lod.temp, id.vars = c("loci", "chr", "pos"))
+        
+        plot.lod= 
+          ggplot(data.lod, aes(x=pos, y=value)) + 
+          geom_line(size = 1 , aes(color = variable))+
+          geom_rug(data=data.lod.markers, 
+                   aes(x=pos), 
+                   sides = "b",
+                   position = "jitter",
+                   alpha = 0.5,
+                   size = 0.25
+          ) + 
+          scale_x_reverse(breaks = seq(0, round(max(chr.size.qtl$size), digits = -1)+5, by = 10),
+                          expansion(mult = c(0, 0.))) +
+          scale_y_continuous(position = "right",
+                             breaks = my_breaks,
+                             lab = my_labs) +
+          coord_flip() +
+          theme_classic() + 
+          theme(
+            strip.text = element_text(size= 15, face = "bold", family=font),
+            strip.background = element_rect(colour = NA, fill = NA),
+            strip.placement = "outside",
+    
+            axis.text = element_text(size =25, face = "bold", family = font, 
+                                     color = "black", margin=margin(0,0,0,15)),
+            text = element_text(size = 25,face = "bold", family = font),
+            axis.line.y = element_line(color = "black", size = 1),
+            legend.position ="bottom",
+            legend.title = element_blank(),
+            legend.box="vertical",
+            legend.text=element_text(size=25),
+            legend.margin=margin(0,0,0,0),
+            legend.box.margin=margin(20,20,20,20)
+          ) + xlab("") + ylab("") + scale_color_manual(values = mypal.qtl)
+    
+        return(plot.lod)
+      }
+    
+    plot.lod = plotLODmqm(qtl.lod.temp)
+    
+    
+
+    # Combine and Save Plots ---------------------------------------------------
+    
+    qtls.plot = plot_grid(qtl.map, plot.lod, rel_widths = c(0.7, 1))
+    
+    save_plot(plot=qtls.plot, 
+              filename = paste(outputplot, prefixResults, ".single_qtl.", LG, ".", int_method, ".pdf", sep = ""),
+              base_height = 30)
+  }
+  
+  message("QTL plots Done!")
+}
